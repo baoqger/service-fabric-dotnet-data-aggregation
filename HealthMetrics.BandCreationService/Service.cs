a4a331864bf981fd5b65a7d54de80d72d1ca015b
Jingjing Bao <JBao6@slb.com>
2023-05-03 16:49:29 +0800

    add comments

--- HealthMetrics.BandCreationService/Service.cs
+++ HealthMetrics.BandCreationService/Service.cs
@@ -42,7 +42,10 @@ namespace HealthMetrics.BandCreationService
         public Service(StatelessServiceContext serviceContext) : base(serviceContext)
         {
         }
-
+        /// <summary>
+        /// This is the main entry point for your service instance.
+        /// </summary>
+        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service instance.</param>
         protected override async Task RunAsync(CancellationToken cancellationToken)
         {
             ConfigurationSettings configSettings = FabricRuntime.GetActivationContext().GetConfigurationPackageObject("Config").Settings;
